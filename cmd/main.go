9f63e7b219d21cc56e79053015e1dcc3e7c75e68
Milinkov Nikita <milinkov.nik@gmail.com>
2025-03-14 22:24:01 +0300

    add quiz and answer handlers

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
 
@@ -9,6 +10,8 @@ import (
 	"github.com/go-telegram/bot/models"
 	"github.com/joho/godotenv"
 )
+
+var selected_quiz = "__none__"
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
@@ -24,7 +27,8 @@ func main() {
 
 	opts := []bot.Option{
 		bot.WithDefaultHandler(defHandler),
-		bot.WithCallbackQueryDataHandler("test", bot.MatchTypePrefix, cbHandler),
+		bot.WithCallbackQueryDataHandler("test", bot.MatchTypePrefix, quizHandler),
+		bot.WithCallbackQueryDataHandler("answer", bot.MatchTypePrefix, answerHandler),
 	}
 
 	b, err := bot.New(TG_API_TOKEN, opts...)
@@ -42,6 +46,7 @@ func startHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		InlineKeyboard: [][]models.InlineKeyboardButton{
 			{
 				{Text: "Общий тест по Linux", CallbackData: "test_main"},
+				{Text: "Текстовые редакторы Linux", CallbackData: "test_txt_editors"},
 			},
 		},
 	}
@@ -60,7 +65,56 @@ func defHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	})
 }
 
-func cbHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+func quizHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: update.CallbackQuery.ID,
+		ShowAlert:       false,
+	})
+
+	switch update.CallbackQuery.Data {
+	case "test_main":
+		selected_quiz = "test_main"
+
+		kb := &models.InlineKeyboardMarkup{
+			InlineKeyboard: [][]models.InlineKeyboardButton{
+				{
+					{Text: "Ответ 1", CallbackData: "answer_1"},
+					{Text: "Ответ 2", CallbackData: "answer_2"},
+					{Text: "Ответ 3", CallbackData: "answer_3"},
+					{Text: "Ответ 4", CallbackData: "answer_4"},
+				},
+			},
+		}
+
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:      update.CallbackQuery.Message.Message.Chat.ID,
+			Text:        "You selected the button: " + update.CallbackQuery.Data,
+			ReplyMarkup: kb,
+		})
+
+	case "test_txt_editors":
+		selected_quiz = "test_txt_editors"
+
+		kb := &models.InlineKeyboardMarkup{
+			InlineKeyboard: [][]models.InlineKeyboardButton{
+				{
+					{Text: "Ответ 1", CallbackData: "answer_1"},
+					{Text: "Ответ 2", CallbackData: "answer_2"},
+					{Text: "Ответ 3", CallbackData: "answer_3"},
+				},
+			},
+		}
+
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:      update.CallbackQuery.Message.Message.Chat.ID,
+			Text:        "You selected the `button`: " + update.CallbackQuery.Data,
+			ReplyMarkup: kb,
+		})
+	}
+
+}
+
+func answerHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
 		CallbackQueryID: update.CallbackQuery.ID,
 		ShowAlert:       false,
@@ -68,6 +122,6 @@ func cbHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.CallbackQuery.Message.Message.Chat.ID,
-		Text:   "You selected the button: " + update.CallbackQuery.Data,
+		Text:   fmt.Sprintf("Your answer for quiz %s was %s", selected_quiz, update.CallbackQuery.Data),
 	})
 }
