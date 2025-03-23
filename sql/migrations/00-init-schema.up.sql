e670494dfaab05b94c30a2a21cbae092e711ca6c
Milinkov Nikita <milinkov.nik@gmail.com>
2025-03-23 21:34:42 +0300

    add 'callback_data' column to quiz table

--- sql/migrations/00-init-schema.up.sql
+++ sql/migrations/00-init-schema.up.sql
@@ -1,22 +1,23 @@
-CREATE TABLE quiz (
+CREATE TABLE IF NOT EXISTS quiz (
     id BIGSERIAL PRIMARY KEY,
-    "name" VARCHAR(64) 
+    "name" VARCHAR(64),
+    callback_data VARCHAR(64) CHECK (callback_data LIKE 'test:%')
 );
 
-CREATE TABLE question (
+CREATE TABLE IF NOT EXISTS question (
     id BIGSERIAL PRIMARY KEY,
     "text" VARCHAR(255),
     quiz_id BIGSERIAL REFERENCES quiz(id)
 );
 
-CREATE TABLE answer (
+CREATE TABLE IF NOT EXISTS answer (
     id BIGSERIAL PRIMARY KEY,
     "text" VARCHAR(32),
     is_right BOOLEAN,
     question_id BIGSERIAL REFERENCES question(id)
 );
 
-INSERT INTO quiz ("name") VALUES ('Базовый тест Linux');
+INSERT INTO quiz ("name", callback_data) VALUES ('Общий тест по Linux', 'test:linux_main');
 INSERT INTO question ("text", quiz_id) VALUES ('Какая команда используется для вывода содержимого папки на экран', 1);
 INSERT INTO answer ("text", question_id, is_right) VALUES ('ls', 1, false);
 INSERT INTO answer ("text", question_id, is_right) VALUES ('cat', 1, true);
